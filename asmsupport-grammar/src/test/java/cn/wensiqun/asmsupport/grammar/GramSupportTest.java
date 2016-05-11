3073a33cc3c8b34150d8ed51cc2d8eb79672bbc8
wsq温斯群 <sqwen@Ctrip.com>
2016-05-11 18:01:23 +0800

    Remove code that's using by javacc.

--- asmsupport-grammar/src/test/java/cn/wensiqun/asmsupport/grammar/GramSupportTest.java
+++ asmsupport-grammar/src/test/java/cn/wensiqun/asmsupport/grammar/GramSupportTest.java
@@ -1,14 +1,12 @@
 package cn.wensiqun.asmsupport.grammar;
 
-import java.io.ByteArrayInputStream;
-
 import org.junit.Test;
 
 public class GramSupportTest {
 
 	@Test
 	public void test() {
-		GramSupport gs = new GramSupport(new ByteArrayInputStream("System.out.println(100);".getBytes()), "UTF-8"); 
+		//GramSupport gs = new GramSupport(new ByteArrayInputStream("System.out.println(100);".getBytes()), "UTF-8");
 	}
 
 }
